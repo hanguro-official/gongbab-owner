097c3156fe43ef62d9bf217a41958a615100c999
devdbsdngml <wh9614y@gmail.com>
2026-01-29 18:15:57 +0900

    Feat: Initialize flutter_screenutil in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gongbab_owner/presentation/router/app_router.dart';
 
 void main() {
@@ -11,15 +12,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      title: 'Gongbab Owner',
-      theme: ThemeData(
-        primaryColor: const Color(0xFF3b82f6),
-        scaffoldBackgroundColor: const Color(0xFF0F172A),
-        fontFamily: 'Pretendard', //
-      ),
-      debugShowCheckedModeBanner: false,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690), // 디자인 시안의 가로, 세로 크기
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          routerConfig: AppRouter.router,
+          title: 'Gongbab Owner',
+          theme: ThemeData(
+            primaryColor: const Color(0xFF3b82f6),
+            scaffoldBackgroundColor: const Color(0xFF0F172A),
+            fontFamily: 'Pretendard', //
+          ),
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
